f6c8d39ea88efba8e6eff300604515e8fe8b5ef3
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2014-02-11 18:36:32 +0000

    Add a getFinalValue function, and refactor tests See http://www.graememcc.co.uk/2014/02/11/functional-parsers-in-javascript-1#prelude

--- Utilities.js
+++ Utilities.js
@@ -7,6 +7,24 @@ define([], function() {
   };
 
 
+  var getFinalValue = function(results) {
+    var resultFound = false;
+    var finalResult = null;
+
+    // Use some to terminate as quickly as possible
+    var findResult = results.some(function(result) {
+      if (result.remaining.length === 0) {
+        resultFound = true;
+        finalResult = result;
+      }
+    });
+
+    if (!resultFound)
+      throw new Error('No result - parse incomplete/failed!');
+    return finalResult.value;
+  };
+
+
   var hasCompleteParse = function(results) {
     return results.some(function(result) {
       return result.remaining.length === 0;
@@ -16,6 +34,7 @@ define([], function() {
 
   return {
     accepts: accepts,
+    getFinalValue: getFinalValue,
     hasCompleteParse: hasCompleteParse
   };
 });
--- test/testUtilities.js
+++ test/testUtilities.js
@@ -25,6 +25,70 @@ requirejs(['Utilities.js', 'chai'], function(Utilities, chai) {
     it('Utilities object\'s hasCompleteParse property is a function', function() {
       expect(Utilities.hasCompleteParse).to.be.a('function');
     });
+
+
+    it('Utilities object has \'getFinalValue\' property', function() {
+      expect(Utilities).to.have.property('getFinalValue');
+    });
+
+
+    it('Utilities object\'s getFinalValue property is a function', function() {
+      expect(Utilities.getFinalValue).to.be.a('function');
+    });
+  });
+
+
+  // The tests for accepts and hasCompleteParse use the following for test-generation
+  // For the incomplete parses, we don't really care what the final value is, so we leave the
+  // second parameter to the makeElement helper undefined
+  var makeElement = function(r, v) {return {remaining: r, value: v};};
+  var tests = [
+    {name: 'array', fail: [makeElement(['a']), makeElement(['b'])],
+     pass: [makeElement(['a']), makeElement(['b']), makeElement([], 'result')], passValue: 'result'},
+    {name: 'string', fail: [makeElement('a'), makeElement('b')],
+     pass: [makeElement('a'), makeElement('b'), makeElement('', 7)], passValue: 7}
+  ];
+
+
+  // The next few tests have a common shape: factor out the boilerplate
+  var makeCommonTests = function(fn, fnName, failTestMaker, successTestMaker) {
+    it(fnName + ' behaves correctly for the empty list', failTestMaker([]));
+
+
+    tests.forEach(function(test) {
+      var name = test.name;
+
+      it(fnName + ' fails if no element with \'remaining\' of length 0 (' + name + ')',
+         failTestMaker(test.fail, test));
+
+      it(fnName + ' succeeds if element with \'remaining\' of length 0 (' + name + ')',
+        successTestMaker(test.pass, test));
+    });
+  };
+
+
+  describe('getFinalValue', function() {
+    var getFinalValue = Utilities.getFinalValue;
+
+
+    var makeFailedParseTest = function(input, test) {
+      return function() {
+        var testFn = function() {
+          getFinalValue(input);
+        };
+        expect(testFn).to.throw(Error);
+      };
+    };
+
+
+    var makeSuccessParseTest = function(input, test) {
+      return function() {
+        expect(getFinalValue(input)).to.equal(test.passValue);
+      };
+    };
+
+
+    makeCommonTests(getFinalValue, 'getFinalValue', makeFailedParseTest, makeSuccessParseTest);
   });
 
 
@@ -32,45 +96,36 @@ requirejs(['Utilities.js', 'chai'], function(Utilities, chai) {
     var hasCompleteParse = Utilities.hasCompleteParse;
 
 
-    // The next few tests have a common shape: factor out the boilerplate
-    var makeHasCompleteParseTest = function(input, expectedParse) {
+    var makeParseTest = function(input, expectedValue) {
       return function() {
-        expect(hasCompleteParse(input)).to.equal(expectedParse);
+        expect(hasCompleteParse(input)).to.equal(expectedValue);
       };
     };
 
 
-    it('HasCompleteParse returns false for the empty list', makeHasCompleteParseTest([], false));
+    var makeFailedParseTest = function(input, test) {
+      return makeParseTest(input, false);
+    };
 
 
-    var makeElement = function(r) {return {remaining: r, value: null};};
-    var tests = [
-      {name: 'array', fail: [makeElement(['a']), makeElement(['b'])], pass: [makeElement(['a']), makeElement(['b']), makeElement([])]},
-      {name: 'string', fail: [makeElement('a'), makeElement('b')], pass: [makeElement('a'), makeElement('b'), makeElement('')]}
-    ];
+    var makeSuccessParseTest = function(input, test) {
+      return makeParseTest(input, true);
+    };
 
 
-    tests.forEach(function(test) {
-      var name = test.name;
-
-      it('HasCompleteParse fails if no element with \'remaining\' of length 0 (' + name + ')',
-         makeHasCompleteParseTest(test.fail, false));
-
-      it('HasCompleteParse succeeds if element with \'remaining\' of length 0 (' + name + ')',
-         makeHasCompleteParseTest(test.pass, true));
-    });
+    makeCommonTests(hasCompleteParse, 'hasCompleteParse', makeFailedParseTest, makeSuccessParseTest);
   });
 
 
   describe('accepts', function() {
     var accepts = Utilities.accepts;
 
-    it('Accepts takes two parameters', function() {
+    it('accepts takes two parameters', function() {
       expect(accepts.length).to.equal(2);
     });
 
 
-    it('Accepts calls the parser with the given input', function() {
+    it('accepts calls the parser with the given input', function() {
       var fakeParser = function(s) {
         fakeParser.param = s;
         fakeParser.called = true;
@@ -85,7 +140,6 @@ requirejs(['Utilities.js', 'chai'], function(Utilities, chai) {
     });
 
 
-    // The next few tests have a common shape: factor out the boilerplate
     var makeAcceptsTest = function(fakeParserOutput, expectedResult) {
       return function() {
         var fakeParser = function(s) {
@@ -98,25 +152,17 @@ requirejs(['Utilities.js', 'chai'], function(Utilities, chai) {
     };
 
 
-    it('Accepts returns false if the parser fails', makeAcceptsTest([], false));
+    var makeFailedParseTest = function(input, test) {
+      return makeAcceptsTest(input, false);
+    };
 
 
-    var makeElement = function(r) {return {remaining: r, value: null};};
-    var tests = [
-      {name: 'array', fail: [makeElement(['a']), makeElement(['b'])], pass: [makeElement(['a']), makeElement(['b']), makeElement([])]},
-      {name: 'string', fail: [makeElement('a'), makeElement('b')], pass: [makeElement('a'), makeElement('b'), makeElement('')]}
-    ];
+    var makeSuccessParseTest = function(input, test) {
+      return makeAcceptsTest(input, true);
+    };
 
 
-    tests.forEach(function(test) {
-      var name = test.name;
-
-      it('Accepts fails if no element with \'remaining\' of length 0 (' + name + ')',
-         makeAcceptsTest(test.fail, false));
-
-      it('Accepts succeeds if element with \'remaining\' of length 0 (' + name + ')',
-         makeAcceptsTest(test.pass, true));
-    });
+    makeCommonTests(accepts, 'accepts', makeFailedParseTest, makeSuccessParseTest);
   });
 });
 
